88cacdff8f31e4806a416d20411adf81ee975dec
Melvin Schmidt <melvin.schmidt92@googlemail.com>
2020-09-01 11:08:29 +0200

    prevent category delete if one memuitem is referenced

--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Category = require('../models/category');
+const MenuItem = require('../models/menuitem')
 const app = express();
 
 app.get('/categories', async (req, res) => {
@@ -25,10 +26,18 @@ app.post('/category', async (req, res) => {
 
   app.delete('/category/:id', async (req, res) => {
     try {
-      const category = await Category.findByIdAndDelete(req.params.id)
-  
-      if (!category) res.status(404).send("No item found")
-      res.status(200).send()
+      const categoryId = req.params.id;
+      let ObjectId = require('mongodb').ObjectId;
+      const c_id = new ObjectId(categoryId);
+      MenuItem.findOne({categoryID: c_id}, async function (err, menuitem) {
+        if(!menuitem){ 
+          const category = await Category.findByIdAndDelete(categoryId);
+          if (!category) res.status(404).send("No item found");
+          res.status(200).send()
+        } else {
+            res.send({status: 'fail', data:{ title: "Kann nicht gelöscht werden, da sich mindestens noch ein Menuitem in dieser Kategorie befindet."}});
+        }
+      })
     } catch (err) {
       res.status(500).send(err)
     }
